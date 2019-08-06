921ba42060542ada71b1ee49cd892403a1275d51
Haris Manzoor <45047495+haris741@users.noreply.github.com>
2019-08-06 17:42:53 +0500

    Completed!

--- app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/xylophonepm/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     private int mGSoundId;
     private int mASoundId;
     private int mBSoundId;
+    SoundPool mSoundPool;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,14 +33,49 @@ public class MainActivity extends AppCompatActivity {
 
         // TODO: Create a new SoundPool
 
-
+        mSoundPool= new SoundPool(7, AudioManager.STREAM_MUSIC, 0);
         // TODO: Load and get the IDs to identify the sounds
-
+        mASoundId=mSoundPool.load(getApplicationContext(), R.raw.note6_a, 1);
+        mBSoundId=mSoundPool.load(getApplicationContext(), R.raw.note7_b, 1);
+        mCSoundId=mSoundPool.load(getApplicationContext(), R.raw.note1_c, 1);
+        mDSoundId=mSoundPool.load(getApplicationContext(), R.raw.note2_d, 1);
+        mESoundId=mSoundPool.load(getApplicationContext(), R.raw.note3_e,1);
+        mFSoundId=mSoundPool.load(getApplicationContext(), R.raw.note4_f, 1);
+        mGSoundId= mSoundPool.load(getApplicationContext(), R.raw.note5_g, 1);
 
     }
 
     // TODO: Add the play methods triggered by the buttons
 
+    public void playA(View v)
+    {
+        mSoundPool.play(mASoundId,1,1,1,0,1);
+    }
+    public void playB(View v)
+    {
+        mSoundPool.play(mBSoundId,1,1,1,0,1);
+    }
+    public void playC(View v )
+    {
+        mSoundPool.play(mCSoundId,1,1,1,0,1);
+    }
+    public void playD(View v)
+    {
+        mSoundPool.play(mDSoundId,1,1,1,0,1);
 
+    }
+    public void playE(View v)
+    {
+        mSoundPool.play(mESoundId,1,1,1,0,1);
+
+    }
+    public void playF(View v )
+    {
+        mSoundPool.play(mFSoundId, 1,1,1,0,1);
+    }
+    public void playG(View v )
+    {
+        mSoundPool.play(mGSoundId,1,1,1,0,1);
+    }
 
 }
